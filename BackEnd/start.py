a1075a017806cd3d9b45d958d37af06905e528d1
Huoyian <huoyian@123.com>
2025-03-03 23:52:02 +0800

    update file

--- BackEnd/start.py
+++ BackEnd/start.py
@@ -4,9 +4,10 @@ from transportation_path import TransportationPath
 app = Flask(__name__)
 path = TransportationPath()
 
-@app.route('/data/recommend_list', methods=['GET'])
-def data_recommend_list():
-    return path.get("2024/5/4-10:00", "潮州", "基隆")
+@app.route('/data/recommend<from_place>-<to_place>', methods=['GET'])
+def data_recommend(from_place, to_place):
+    val = path.get("2024/5/4-10:00", from_place, to_place)
+    return val
 
 if __name__ == '__main__':
     v = path.get("2024/5/4-10:00", "潮州", "基隆")
