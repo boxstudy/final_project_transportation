de25269a873546f93404b4f92de79b6d3cdfc5b1
Huoyian <huoyian@123.com>
2025-03-05 14:29:14 +0800

    fix some bugs

--- BackEnd/start.py
+++ BackEnd/start.py
@@ -4,14 +4,15 @@ from transportation_path import TransportationPath
 app = Flask(__name__)
 path = TransportationPath()
 
-@app.route('/data/recommend<from_place>-<to_place>', methods=['GET'])
-def data_recommend(from_place, to_place):
-    val = path.get("2024/5/4-10:00", from_place, to_place)
+@app.route('/data/recommend<time>_<from_place>_<to_place>', methods=['GET'])
+def data_recommend(time, from_place, to_place):
+    val = path.get(time, from_place, to_place)
     return val
 
 if __name__ == '__main__':
 
-    v = path.get("2024/5/4-10:00", "潮州", "基隆")
-    print(v)
+    if app.debug:
+        v = path.get("2024/5/4-10:00", "潮州", "基隆")
+        print(v)
 
     app.run(threaded=True, port=8888, debug=True, host='0.0.0.0')
